d2ff9e10087a784f601ee7d4594b06e6d127c187
Ricky-patrick <patrick.fernandex3@gmail.com>
2021-03-05 03:09:56 +0700

    design dan perampingan coding

--- game-suit/javascript.js
+++ game-suit/javascript.js
@@ -1,34 +1,37 @@
-//pilihan player
-var player = prompt('batu,gunting,kertas');
+function computer() {
 
-//angka random komputer
-var computer = Math.random();
+    const computer = Math.random();
 
-if( computer < 0.33 ) {
-    computer = 'batu';
-} else if( computer >= 0.33 && computer < 0.75) {
-    computer = 'kertas';
-} else {
-    computer = 'gunting';
-}
-//aturan
-var hasil = '';
-
-if( player == computer ) {
-    hasil = 'seri';
-} 
-else if( player == 'batu' ) {
-    hasil = ( computer == 'gunting' ) ? 'menang' : 'kalah';
-}
-else if( player == 'gunting') {
-    hasil = ( computer == 'kertas') ? 'menang' : 'kalah';
-}
-else if( player == 'kertas') {
-    hasil = ( computer == 'batu') ? 'menang' : 'kalah';
-}
-else {
-    hasil = 'silahkan memilih pilihan batu , gunting , atau kertas jangan masukkan yang lain!';
+    if( computer < 0.33 ) return 'batu';
+    if( computer >= 0.33 && computer < 0.75 ) return 'kertas';
+    return 'gunting';
 }
 
-//hasil
-alert('player :' + player + '\ncomputer :' + computer + '\nhasil :' + hasil);
+function hasil(computer,player) {
+
+    if( player == computer ) return 'seri'; 
+
+    if( player == 'batu' ) return ( computer == 'gunting' ) ? 'menang' : 'kalah';
+
+    if( player == 'gunting') return ( computer == 'kertas') ? 'menang' : 'kalah';
+
+    if( player == 'kertas') return ( computer == 'batu') ? 'menang' : 'kalah';
+}
+
+
+const pilihan = document.querySelectorAll('button img');
+pilihan.forEach(function(pilih){
+    pilih.addEventListener('click',function(){
+
+        const hasilplayer = pilih.className;
+        const hasilcomputer = computer();
+        const hasilakhir = hasil(hasilcomputer,hasilplayer);
+        
+        const gbrcomputer = document.querySelector('.gbr-computer');
+        gbrcomputer.setAttribute('src', 'gambar/' + hasilcomputer + '.png');
+        
+        const hasilpermainan = document.querySelector('.hasilpermainan');
+        hasilpermainan.innerHTML = hasilakhir;
+    })
+});
+
